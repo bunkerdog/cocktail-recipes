1116ac31d3e76cc4159d2b059052635807e576d2
bunkerdog <franknfurter2002@protonmail.com>
2023-06-27 07:47:50 -0600

    renamed repo

--- script.js
+++ script.js
@@ -2,4 +2,29 @@
 this is a javascreipt for a cocktail page
 */
 
+//variables
+const cocktailListUrl="www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+const cocktailList=document.getElementById("cocktail-List");
+window.addEventListener("load", updateCocktailList );
 
+function getCocktailList(){
+    return fetch(cocktailListUrl).then(response => response.json());
+}
+
+
+function updateCocktailList(){
+    getCocktailList().then(function(data){
+        for (element in data.message){
+            let option =createOption(element);
+            cocktailList.appendChild(option);
+        }
+    }
+
+    );
+}
+
+function createOption(text){
+    let option = document.createElement("option");
+    option.textContent=text;
+    return option;
+}
